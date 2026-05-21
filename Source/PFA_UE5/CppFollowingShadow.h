791883d40a503e12b59fdf40c1fbae3fd3c310c3
Jules Petit Faucher <jules.petitfaucher@gmail.com>
2026-05-21 09:27:23 +0200

    Merge Pour Khanthein

---
+++ Source/PFA_UE5/CppFollowingShadow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CppDeathZone.h"
+#include "CppFollowingShadow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PFA_UE5_API ACppFollowingShadow : public ACppDeathZone
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PFA_UE5/CppShadowMonsters.cpp
@@ -0,0 +1,2 @@
+#include "D:\UnrealProject\PAF_UE5\Intermediate\Build\Win64\x64\PFA_UE5Editor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_3.h"
+#include "CppShadowMonsters.h"
